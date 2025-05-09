1e5ffc14c369466785d88f11bf07ab0db37a73e2
boomika1309 <boomika1309@gmail.com>
2025-05-09 14:06:18 +0530

    pythonFirstClass

---
+++ FirstClass.py
@@ -0,0 +1,47 @@
+#printStart
+print("Hello Haii")
+#PrintEnd
+
+
+"""
+
+23456[
+asdfghjkl;
+asdfghjkl
+asdfghjkl;
+
+"""
+#1Name X
+#Name1✔
+#fist Name X
+
+#firstName
+#FirstName
+#First-Neme X
+#First_NAme
+#_First_Name
+
+"""
+Camelcase :
+        myVariableName
+
+PascalCase :
+        MyVariableName
+SnakeCase :
+
+_My_Variable_Name
+
+"""
+a,b,c = 1,"surya",1.3
+
+print(a)
+print(b)
+print(c)
+
+e=f=g="IDMTECHPARK"
+
+print(e)
+print(f)
+print(g)
+ 
+
