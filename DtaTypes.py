ecf4088a610ccd629a1668697df998c3f1221bf5
boomika1309 <boomika1309@gmail.com>
2025-05-12 13:58:44 +0530

    DataTypes

---
+++ DtaTypes.py
@@ -0,0 +1,23 @@
+#Number = int ,float,complex
+
+#int = 12,0 ,123456788 ,-1234  -n 0 n ;
+
+
+#float = 1.2,123456.6,-12.4   -n.n  0.0 n.n
+
+#complex = 1j 1+5j
+
+String = 'surya'   "surya" "
+Sequence = LIST [],Array,Tuples [1,2.2,'s',"surya",Trues]
+
+Map = dis{ NAME : "SURya",
+           AGE:12,
+           CITY:"Erode"
+           }
+
+Booloan = True 1 ,False  0;
+
+
+None : None
+
+
---
+++ Example.py
@@ -0,0 +1,51 @@
+print(10<30)
+ 
+#  a= 10 ;
+    #a==12
+
+#Type Casting
+
+a=1
+b=float(a)
+c=complex(a)
+print(b)
+print(c)
+
+
+#List
+
+Number=[1,2,3,4,True,"Surya"]
+
+print(Number[4])
+
+#negativeIntex
+print(Number[-1])
+
+#Range
+print(Number[0:4])
+
+#startrange
+print(Number[1:])
+
+#End Range
+print(Number[:4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
